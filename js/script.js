a0405b06fa4e5d07ff844b3cd82ddf22ac3db382
UtkarshBhimte <utkarshbhimte95@gmail.com>
2016-09-06 01:27:36 +0530

    added all the com-logos

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    var logo = [
+
+        "accenture.jpg", "Accord.jpg", "alphaninemarine.png", "amazon.png", "aptean.png", "aricent.png", "aris_global.png", "Aryaka_logo.png", "at&t.jpg", "betsol.png", "bookmyshow.png", "bosch-logo.jpg", "broadsoft.jpg", "CelstreamLogo.jpg", "Cerner.jpg", "cgi-logo.png", "cocubes.png", "deltax.png", "ge-logo-retro.jpg", "HCL.png",
+        "HealthAsyst_Logo.JPG", "hgs.png", "hiveminds.png", "hp.png", "hpenterprise.png", "Huawei.png", "ibm_logo.gif", "imshealth.jpg", "infisys.png", "inteva_products_logo.jpg", "l&tinfo.jpg", "logo160.gif", "logo_OdessaTechnologies300x130.gif", "metric.png", "mindtree.jpg", "moog logo.jpg", "mphasis.jpg", "MuSig.jpg", "NEC_logo.png", "ninjacart.jpeg", "nokia-logo.jpg", "NTT_DATA_Logo.jpg", "pwc.jpg", "Rare-Mile-Technologies-Logo.jpg", "Rothenberger-logo.jpg", "Samsung_Logo.svg.png", "Sonata-software.jpg", "Tech-Mahindra-Logo-Photos.jpg", "ThoughtWorks-logo-transpare.png", "torry.gif", "toyota.jpg", "trivenilogo.jpg", "utc-logo.png", "wipro-logo.jpg",
+        "alphaninemarine.png", "amazon.png", "aptean.png", "aricent.png", "aris_global.png", "Aryaka_logo.png", "at&t.jpg", "betsol.png", "bookmyshow.png", "bosch-logo.jpg", "broadsoft.jpg", "CelstreamLogo.jpg", "Cerner.jpg", "cgi-logo.png", "cocubes.png", "deltax.png", "ge-logo-retro.jpg", "HCL.png", "HealthAsyst_Logo.JPG", "hgs.png", "hiveminds.png", "hp.png", "hpenterprise.png", "Huawei.png", "ibm_logo.gif", "imshealth.jpg", "infisys.png", "inteva_products_logo.jpg", "l&tinfo.jpg", "logo160.gif", "logo_OdessaTechnologies300x130.gif", "metric.png", "mindtree.jpg", "moog logo.jpg", "mphasis.jpg", "MuSig.jpg", "ninjacart.jpeg", "nokia-logo.jpg", "NTT_DATA_Logo.jpg", "pwc.jpg", "Rare-Mile-Technologies-Logo.jpg", "Rothenberger-logo.jpg", "Samsung_Logo.svg.png", "Sonata-software.jpg", "Tech-Mahindra-Logo-Photos.jpg", "ThoughtWorks-logo-transpare.png", "torry.gif", "toyota.jpg", "trivenilogo.jpg", "utc-logo.png", "wipro-logo.jpg",
+    ];
+
+    $.each(logo,function(index, el) {
+
+      $('#past-recruitment-logo').append('<img class="col-md-2" src="images/com-logos/' + el + '" alt="'+ el + '" >');
+
+    });
+
+});
